08b6f79a37da00c1d67233ab8c6bea21a043854e
MajesticeM <67010913+MajesticeM@users.noreply.github.com>
2021-05-18 19:40:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(()=>{
+    $('#searchForm').on('submit',(e)=>{
+   let searchText=$('#searchText').val();
+   getMovies(searchText);
+    e.preventDefault()
+    });
+ });
+    
+function getMovies(searchText) {
+   axios.get('http://www.omdbapi.com?s='+searchText)
+   .then((response)=>{
+    console.log(response)
+   })
+   .catch((err)=>{
+        console.log(err);
+   });
+}
